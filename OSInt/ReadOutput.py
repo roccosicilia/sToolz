e9e7307be085dd281ce148d77f2abe72c4973fc9
roccosicilia <rocco.sicilia@protonmail.com>
2021-11-01 22:42:34 +0100

    test

--- OSInt/ReadOutput.py
+++ OSInt/ReadOutput.py
@@ -50,4 +50,4 @@ else:
     ### print file list
     if sys.argv[1] == '-f':
         for result in results["organic_results"]:
-            print(result['about_this_result'])
+            print(result['about_this_result']['source'])
