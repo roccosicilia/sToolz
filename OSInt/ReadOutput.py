502ef5ffafa2dd850324104ccdd73006b256c73f
roccosicilia <rocco.sicilia@protonmail.com>
2021-11-01 22:20:17 +0100

    get data

--- OSInt/ReadOutput.py
+++ OSInt/ReadOutput.py
@@ -12,6 +12,21 @@ import json
 with open('data.json') as json_file:
     results = json.load(json_file)
 
-    for data in results:
-        print(data)
+    print("####################")
+    print(results["search_metadata"])
 
+    print("####################")
+    print(results["search_parameters"])
+
+    print("####################")
+    print(results["search_information"])
+
+    print("####################")
+    print(results["organic_results"])
+
+    print("####################")
+    print(results["pagination"])
+
+    print("####################")
+    print(results["serpapi_pagination"])
+    
