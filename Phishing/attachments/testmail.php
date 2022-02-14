750f1395ad201ff3c82ca85b727bcbe61a2701e1
roccosicilia <rocco.sicilia@protonmail.com>
2022-02-14 16:11:47 +0100

    test email

--- Phishing/attachments/testmail.php
+++ Phishing/attachments/testmail.php
@@ -1,11 +1,13 @@
 <?php
 
+/* test - work in progress */
+
 $sender = "";
 $repicient = "";
 $subject = "Test mail from php";
 $headers = "From: $sender\r\n";
-$headers += "Reply-To: $sender\r\n";
-$headers += "X-Mailer: PHP/" . phpversion();
+$headers .= "Reply-To: $sender\r\n";
+$headers .= "X-Mailer: PHP");
 
 # content
 $username = "Test User";
@@ -13,9 +15,9 @@ $link = "";
 $hidden_content = "";
 
 $message = "";
-$message += "Hi $username, \r\n";
-$message += "Link: $link";
-$message += "Hidden content: ";
+$message .= "Hi $username, \r\n";
+$message .= "Link: $link";
+$message .= "Hidden content: ";
 
 mail($repicient, $subject, $message, $headers);
 
