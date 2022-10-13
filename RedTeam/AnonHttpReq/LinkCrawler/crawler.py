2e4ca4166488435efc00f245aea70e8a9e5e4066
roccosicilia <rocco.sicilia@protonmail.com>
2022-10-13 18:12:57 +0200

    crawler one-level

--- RedTeam/AnonHttpReq/LinkCrawler/crawler.py
+++ RedTeam/AnonHttpReq/LinkCrawler/crawler.py
@@ -2,23 +2,35 @@
 #
 # Get data link from a website
 # Author: Rocco <Sheliak> Sicilia
-# Usage: $ 
+# Usage: $ python ./crawler.py {target} {filename}
 #
 ###################################################################################################
 
 import sys
-import json
 import requests
-import readline
 
 # vars
 target = sys.argv[1]
+filename = sys.argv[2]
 
 content = requests.get(target)
 content_html = content.text
 #print(content_html)
 
-lines = readline(content_html)
+lines = content_html.splitlines()
 for line in lines:
-    if 'href' in line:
-        print(line)
+    if 'href=' in line:
+        # print(line.strip())
+        string = line.split(' ')
+        for element in string:
+            # print(element)
+            if 'href=' in element:
+                # print(element)
+                link = element.split('>')
+                link = link[0].replace('href=', '')
+                link = link.replace("'", '')
+                link = link.replace('"', '')
+                if len(link) > 3 and target in link:
+                    # print(link)
+                    with open("{}".format(filename), "a") as output:
+                        output.write(link + "\r\n")
