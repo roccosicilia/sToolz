b0b38992fe43a24eb07826632312676f1f0945df
roccosicilia <rocco.sicilia@protonmail.com>
2022-10-15 11:34:41 +0200

    add header

--- RedTeam/AnonHttpReq/AnonHttpReq.py
+++ RedTeam/AnonHttpReq/AnonHttpReq.py
@@ -16,6 +16,7 @@ target = sys.argv[1]
 urlfile = sys.argv[2]
 counter = int(sys.argv[3])
 session = requests.session()
+useragent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'}
 
 urls = open(urlfile).readlines()
 urls = ' '.join(urls).split()
@@ -28,5 +29,5 @@ while (i <= counter):
     print("Select URL in pos {}, proxy {}: \t{}".format(pos, socksp, urls[pos]))
     # session.get(urls[pos]).text
     session.proxies = { 'http':  'socks5://192.168.1.6:906{}'.format(socksp), 'https': 'socks5://192.168.1.6:906{}'.format(socksp) }
-    session.get(urls[pos])
+    session.get(urls[pos], headers = useragent)
     i = i+1
