bc77f4084f354139f927e8a8c478d10d8f15d962
roccosicilia <rocco.sicilia@protonmail.com>
2023-01-10 01:20:05 +0100

    pwd

--- RedTeam/C2demo/demo.sh
+++ RedTeam/C2demo/demo.sh
@@ -17,7 +17,7 @@ then
     echo "NOP"
 else
     # do something
-    eval $CMD >> ./output.txt
+    eval $CMD >> $BASEDIR/RedTeam/C2demo/output.txt
     # commit and push
     git add .
     git commit -m "NOP"
