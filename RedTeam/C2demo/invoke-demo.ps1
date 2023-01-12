e7a55b9e37b6963fbf7aa577473d748f60cbec0f
roccosicilia <rocco.sicilia@protonmail.com>
2023-01-12 10:48:10 +0100

    NOP

--- RedTeam/C2demo/invoke-demo.ps1
+++ RedTeam/C2demo/invoke-demo.ps1
@@ -3,7 +3,7 @@
 $cmd = Invoke-webrequest -URI "https://raw.githubusercontent.com/roccosicilia/sToolz/master/RedTeam/C2demo/input.txt"
 Write-Host "Test del comando" + $cmd
 
-if ( $cmd == 'NOP')
+if ( $cmd -eq 'NOP')
 {
     Write-Host $cmd
 }
